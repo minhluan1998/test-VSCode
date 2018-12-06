1baa06c21c6e091ad01b017d3b1bdf8c83ff932b
minhluan1998 <minhluan1998@gmail.com>
2018-12-06 23:57:01 +0700

    Commit Now

--- test.js
+++ test.js
@@ -1,16 +1,2 @@
 document.getElementById("oke");
 document.getElementById('asdasdkjasd');
-window.alert("I do what i want hahaahahahahah");
-
-
-
-
-
-
-aaaaaaaaaaaaaaaaaaaaaa
-aaaaaaaaaaaaaaaaaaaaaaaaaaa
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-
-aaaaaaaaaaaaaaaaaaaaaa
