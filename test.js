7d12ca03b25106513be8d61136c7e397d17203c9
minhluan1998 <minhluan1998@gmail.com>
2018-12-06 23:54:59 +0700

    change it now bae

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
 document.getElementById("oke");
 document.getElementById('asdasdkjasd');
 window.alert("I do what i want hahaahahahahah");
+
+
+
+
+
+
+aaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+
+aaaaaaaaaaaaaaaaaaaaaa
